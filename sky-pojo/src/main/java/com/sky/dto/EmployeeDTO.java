07c0ee1c8218f293cfbab7d63c6ce37141567012
cewly <1624350589@q.com>
2025-11-17 22:19:23 +0800

    新增员工业务代码

--- sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -1,12 +1,16 @@
 package com.sky.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@ApiModel(description = "员工DTO")
 public class EmployeeDTO implements Serializable {
 
+    @ApiModelProperty(value = "员工ID", example = "null")
     private Long id;
 
     private String username;
